5a2f3da2a993f4800404d54e248436dca32845f0
Ross Salge <rossnsalge@gmail.com>
2017-10-04 16:30:13 -0500

    functional-shared stack apssing all but three tests

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,33 @@
+
+
+
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.lineLength = 0;
+  _.extend(someInstance, stackMethods);
+  return someInstance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.storage[this.lineLength]=value;
+  this.lineLength++;
+}
 
+stackMethods.pop = function() {
+  if(this.lineLength === 0){
+    return;
+  } else{
+    var value = this.storage[this.lineLength];
+    delete this.storage[this.lineLength];
+    this.lineLength--;
+    return value;
+  }
+
+}
+
+stackMethods.size = function() {
+  return this.lineLength;
+}
