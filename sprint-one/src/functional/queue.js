ea6a8d8260f0ed728095ebf3f7d06f30dd608f84
Ross Salge <rossnsalge@gmail.com>
2017-10-04 12:04:46 -0500

    All tests passing for funcitonal stack and queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,12 +5,15 @@ var Queue = function() {
   var storage = {};
   var size = 0;
   var placeInQueue = 1;
+  var key = 0;
+  var endOfLine = 1;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
     size++;
-    storage[size] = value;
+    storage[endOfLine] = value;
+    endOfLine++;
   };
 
   someInstance.dequeue = function() {
@@ -20,9 +23,7 @@ var Queue = function() {
     var result = storage[placeInQueue];
     delete storage[placeInQueue];
     size--;
-    if( size !== 0 ) {
-      placeInQueue++;
-    }
+    placeInQueue++;
     return result;
   };
 
