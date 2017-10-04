18442135ff64c36714edf15354a3cd92f5fb3f9d
Ross Salge <rossnsalge@gmail.com>
2017-10-04 18:48:41 -0500

    fucnitonal-shared stack passing all tests

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -12,22 +12,22 @@ var Stack = function() {
 var stackMethods = {};
 
 stackMethods.push = function(value) {
-  this.storage[this.lineLength]=value;
   this.lineLength++;
-}
+  this.storage[this.lineLength] = value;
+};
 
 stackMethods.pop = function() {
-  if(this.lineLength === 0){
+  if (this.lineLength === 0) {
     return;
-  } else{
+  } else {
     var value = this.storage[this.lineLength];
     delete this.storage[this.lineLength];
     this.lineLength--;
     return value;
   }
 
-}
+};
 
 stackMethods.size = function() {
   return this.lineLength;
-}
+};
