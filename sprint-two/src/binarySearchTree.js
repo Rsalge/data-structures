d824179b918b17166fa0a6cef5b173372703e801
Ross Salge <rossnsalge@gmail.com>
2017-10-05 12:41:38 -0500

    adjusted binarySearchTreeSpec to use psuedoclassical form, started on binarySearchTree and it is passing the first test

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,49 @@
 var BinarySearchTree = function(value) {
+  this.value = value;
+  this.left = undefined;
+  this.right = undefined;
+
 };
 
+BinarySearchTree.prototype.insert = function(value) {
+  //places a value into a specific index
+  var leftOrRight = function(value, node) {
+    if (value > node){
+      if (node.right === undefined) {
+        node.right.value = value;
+      } else if (value > node.right) {
+        leftOrRight(value, node.right);
+      }
+    } else if (value < node) {
+      if (node.left === undefined) {
+        node.left.value = value;
+      } else if (value < node.left) {
+        leftOrRight(value, node.left);
+      }
+    }
+  };
+  if (value > this.value) {
+    if (this.right === undefined) {
+      this.right.value = value;
+    } else if (value > this.right) {
+      leftOrRight(value, this.right);
+    }
+  } else if (value < this.value) {
+    if( this.left === undefined) {
+      this.left['value'] = value;
+    } else if (value < this.left) {
+      leftOrRight(value, this.left);
+    }
+  }
+};
+
+BinarySearchTree.prototype.contains = function(value) {
+  //searches tree for value
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(callback) {
+  //accepts a callback function and applys it to all the values in the tree
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
