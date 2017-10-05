447580d27584cfc57326ac580e3c014b5acc7ec7
Ross Salge <rossnsalge@gmail.com>
2017-10-05 17:29:13 -0500

    BinarySearchTree: passing 2 tests

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,44 +1,53 @@
 var BinarySearchTree = function(value) {
   this.value = value;
-  this.left = undefined;
-  this.right = undefined;
+  this.left = null;
+  this.right = null;
 
 };
 
-BinarySearchTree.prototype.insert = function(value) {
+BinarySearchTree.prototype.insert = function(value, node) {
   //places a value into a specific index
   var leftOrRight = function(value, node) {
-    if (value > node) {
-      if (node.right === undefined) {
-        node.right.value = value;
-      } else if (value > node.right) {
+    if (value > node.value) {
+      if (node.right === null) {
+        node.right = new BinarySearchTree(value);
+      } else if (value > node.right.value) {
         leftOrRight(value, node.right);
       }
-    } else if (value < node) {
-      if (node.left === undefined) {
-        node.left.value = value;
-      } else if (value < node.left) {
+    } else if (value < node.value) {
+      if (node.left === null) {
+        node.left = new BinarySearchTree(value);
+      } else {
         leftOrRight(value, node.left);
       }
     }
   };
   if (value > this.value) {
-    if (this.right === undefined) {
-      this.right.value = value;
-    } else if (value > this.right) {
-      leftOrRight(value, this.right);
+    if (this.right === null) {
+      this.right = new BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
     }
   } else if (value < this.value) {
-    if ( this.left === undefined) {
-      this.left['value'] = value;
-    } else if (value < this.left) {
-      leftOrRight(value, this.left);
+    if ( this.left === null) {
+      this.left = new BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
     }
   }
 };
 
 BinarySearchTree.prototype.contains = function(value) {
   //searches tree for value
+  if(this.value === value) {
+    return true;
+  } else if (this.right.value === value) {
+    return true;
+  } else if (this.left.value === value) {
+    return true;
+  } else if(value > this.right.value) {
+
+  }
 };
 
 BinarySearchTree.prototype.depthFirstLog = function(callback) {
