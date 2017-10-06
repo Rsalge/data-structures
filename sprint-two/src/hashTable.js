dcd89bf45953ae2ef1c48ab0a6ada4d8442b832a
rsalge <rossnsalge@gmail.com>
2017-10-06 15:54:31 -0500

    Hash Table: All tests passing

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,17 +7,41 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage[index] = v;
+  var existing = this._storage.get(index);
+  if(existing) {
+    for (var i = 0; i < existing.length; i++) {
+      if (existing[i][0] === k) {
+        existing[i][1] = v;
+        this._storage.set(index, existing);
+        return;
+      }
+    }
+    existing.push([k,v]);
+    this._storage.set(index, existing);
+  } else {
+    this._storage.set(index, [[k,v]]);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage[index];
+  var existing = this._storage.get(index);
+  if(existing) {
+    for (var i = 0; i < existing.length; i++) {
+      if( existing[i][0] === k ) {
+        return existing[i][1];
+      }
+    }
+  } else {
+    if ( existing ) {
+      return this._storage.get(index);
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  delete this._storage[index];
+  this._storage.set(index, undefined);
 };
 
 
